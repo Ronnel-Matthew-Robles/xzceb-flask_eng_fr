45fa9ec878970942e8e4ac46c3ea2cdd5a1c2da3
Ronnel-Matthew-Robles <mattwolfrob@gmail.com>
2022-08-04 08:21:03 +0000

    Final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from translator import english_to_french, french_to_english
+from ..translator import english_to_french, french_to_english
 
 class TestEnglishToFrench(unittest.TestCase): 
     def test1(self): 
